71d1c86fd8f2ef1fc72447875f588976ebd6eb94
Florens Wasserfall <wasserfall@kalanka.de>
2016-04-13 16:48:56 +0200

    rubberbanding objects in Electronics-3D-Scene

--- lib/Slic3r/GUI/Electronics3DScene.pm
+++ lib/Slic3r/GUI/Electronics3DScene.pm
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use utf8;
 
+use OpenGL qw(:glconstants :glfunctions :glufunctions :gluconstants);
 use Wx::Event qw(EVT_MOUSE_EVENTS);
 use base qw(Slic3r::GUI::3DScene);
 
@@ -12,7 +13,7 @@ use Data::Dumper;
 # Purpose    : Creates a new 3D scene
 # Parameters : see Slic3r::GUI::3DScene
 # Returns    : see Slic3r::GUI::3DScene
-# Commet     :
+# Comment     :
 #######################################################################
 sub new {
     my ($class, $parent) = @_;
@@ -25,11 +26,60 @@ sub new {
     return $self;
 }
 
+sub load_scene_volume {
+	my ($self) = @_;
+}
+
+sub draw_line {
+	my ($self, $a, $b, $width, $color) = @_;
+	
+	# default color is black
+	if(!defined $color) {
+		$color = [0, 0, 0, 0.8];
+	}
+	
+	my $tverts = Slic3r::GUI::_3DScene::GLVertexArray->new;	
+	
+	my $dx = $b->x-$a->x;
+    my $dy = $b->y-$a->y;
+    my $vector_l = sqrt($dx**2+$dy**2);
+    my $x_off = ($dy*$width/2)/$vector_l;
+    my $y_off = ($dx*$width/2)/$vector_l;
+    
+    # Translate points to origin
+    $a->translate($self->origin->x, $self->origin->y, 0);
+    $b->translate($self->origin->x, $self->origin->y, 0);
+    
+	$tverts->push_vert($a->x+$x_off, $a->y-$y_off, $a->z);
+	$tverts->push_norm(0, 0, 1);
+	$tverts->push_vert($b->x+$x_off, $b->y-$y_off, $b->z);
+	$tverts->push_norm(0, 0, 1);
+	$tverts->push_vert($a->x-$x_off, $a->y+$y_off, $a->z);
+	$tverts->push_norm(0, 0, 1);
+	
+	$tverts->push_vert($a->x-$x_off, $a->y+$y_off, $a->z);
+	$tverts->push_norm(0, 0, 1);
+	$tverts->push_vert($b->x+$x_off, $b->y-$y_off, $b->z);
+	$tverts->push_norm(0, 0, 1);
+	$tverts->push_vert($b->x-$x_off, $b->y+$y_off, $b->z);
+	$tverts->push_norm(0, 0, 1);
+
+    my $bb = Slic3r::Geometry::BoundingBoxf3->new;
+    $bb->merge_point($a);
+    $bb->merge_point($b);
+
+    push @{$self->volumes}, Slic3r::GUI::3DScene::Volume->new(
+        bounding_box    => $bb,
+        color           => $color,
+        tverts			=> $tverts,
+    );
+}
+
 #######################################################################
 # Purpose    : Processes mouse events
 # Parameters : An mouse event
 # Returns    : none
-# Commet     : Overloads the method mouse_event_new of Slic3r::GUI::3DScene
+# Comment     : Overloads the method mouse_event_new of Slic3r::GUI::3DScene
 #######################################################################
 sub mouse_event_new {
     my ($self, $e) = @_;
